57cad416f772e15f42b084366b03e94de0dd3b8b
ElusiveJames <86646115+ElusiveJames@users.noreply.github.com>
2024-06-18 08:11:03 +0300

    first tracer bullet

---
+++ _printf.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include <string.h>
+#include "main.h"
+#include <unistd.h>
+/**
+* _printf - function that prints a formatted string
+* @format: format string
+* Return: number of characters printed
+*/
+
+int _printf(const char *format, ...)
+{
+	char *f_str = NULL;
+	int f_int = 0;
+	char f_char;
+
+	va_list list;
+
+	va_start(list, format);
+
+	if (format == NULL)
+		return (-1);
+
+	while  (*format != '\0')
+	{
+		if (*format == '%')
+		{
+			format++;
+			switch (*format)
+			{
+				case 's':
+					f_str = va_arg(list, char *);
+					f_int += write(1, f_str, strlen(f_str));
+					break;
+				case 'c':
+					f_char = va_arg(list, int);
+					f_int += write(1, &f_char, 1);
+					format++;
+					break;
+				default:
+					f_int += write(1, format, 1);
+					break;
+			}
+		}
+		f_int += write(1, format, 1);
+		format++;
+	}
+	va_end(list);
+	return (f_int);
+
+}
---
+++ main.h
@@ -0,0 +1,8 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+#include <stdlib.h>
+#include <stdarg.h>
+
+int _printf(const char *format, ...);
+
+#endif
